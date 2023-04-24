13fc9f4c017d27963d8dd300c8f81c6b618153c9
Ung Nguyen <commitment1511@outlook.com>
2023-04-25 00:20:28 +0700

    add auth guard

--- src/modules/cart/cart.controller.ts
+++ src/modules/cart/cart.controller.ts
@@ -1,17 +1,25 @@
-import { Controller } from '@nestjs/common'
+import { Controller, UseGuards } from '@nestjs/common'
 import { CartService } from './cart.service'
 import { MessagePattern } from '@nestjs/microservices'
 import { CART_PATTERN } from '@src/utils/enums/cart.enum'
+import { Roles } from '@src/utils/decorators/role.decorator'
+import { role } from '@src/utils/enums/role.enum'
+import { JwtAuthGuard } from '@src/utils/guards/jwt.guard'
+import { RolesGuard } from '@src/utils/guards/roles.guard'
 
 @Controller()
 export class CartController {
     constructor(private readonly cartService: CartService) {}
 
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(role.admin, role.customer)
     @MessagePattern(CART_PATTERN.cart_create)
     async createCart(data) {
         return this.cartService.createCart(data)
     }
 
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(role.admin, role.customer)
     @MessagePattern(CART_PATTERN.cart_get_all)
     async getAllCart({ page = 1, size = 10 }) {
         const take = size
@@ -19,16 +27,22 @@ export class CartController {
         return this.cartService.getAllCart({ take, skip })
     }
 
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(role.admin, role.customer)
     @MessagePattern(CART_PATTERN.cart_get_one)
     async getCart(id: string) {
         return this.cartService.getCart(id)
     }
 
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(role.admin, role.customer)
     @MessagePattern(CART_PATTERN.cart_update)
     async updateCart(data) {
         return this.cartService.updateCart(data)
     }
 
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(role.admin, role.customer)
     @MessagePattern(CART_PATTERN.cart_delete)
     async deleteCart(id: string) {
         return this.cartService.deleteCart(id)
