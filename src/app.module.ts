405b74615df155b94df454fe3c5be8c5dcce2c69
Ung Nguyen <commitment1511@outlook.com>
2023-05-02 17:47:59 +0700

    update role

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { ORMModule } from './core/database/orm.module'
 import { ConfigModule } from '@nestjs/config'
 import { OrdertModule } from './modules/order/order.module'
 import { CarttModule } from './modules/cart/cart.module'
 
 @Module({
-    imports: [ConfigModule.forRoot(), ORMModule, OrdertModule, CarttModule],
-    controllers: [AppController],
-    providers: [AppService],
+    imports: [ConfigModule.forRoot(), ORMModule, OrdertModule, CarttModule]
 })
 export class AppModule {}
