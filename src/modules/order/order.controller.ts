a36f35e65f56eaec605a4f519ee9581ba71040cf
milkyway <milkyway1511@outlook.com>
2024-01-13 14:48:34 +0700

    change to http server

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -1,7 +1,5 @@
-import { Controller, UseGuards } from '@nestjs/common'
+import { Body, Controller, Param, Post, Req, UseGuards } from '@nestjs/common'
 import { OrderService } from './order.service'
-import { MessagePattern } from '@nestjs/microservices'
-import { ORDER_PATTERN } from '@src/utils/enums/order.enum'
 import { Roles } from '@src/utils/decorators/role.decorator'
 import { role } from '@src/utils/enums/role.enum'
 import { JwtAuthGuard } from '@src/utils/guards/jwt.guard'
@@ -13,8 +11,8 @@ export class OrderController {
 
     @UseGuards(JwtAuthGuard, RolesGuard)
     @Roles(role.admin, role.customer)
-    @MessagePattern(ORDER_PATTERN.order_create)
-    async createOrder(data) {
+    @Post('create')
+    async createOrder(@Body() data) {
         const decoded = jwt.decode(data.token)
         data.user = decoded
         return this.orderService.createOrder(data)
@@ -22,8 +20,8 @@ export class OrderController {
 
     @UseGuards(JwtAuthGuard, RolesGuard)
     @Roles(role.admin, role.customer)
-    @MessagePattern(ORDER_PATTERN.order_get_all)
-    async getAllOrder({ page = 1, size = 10, token }) {
+    @Post('get-all-order')
+    async getAllOrder(@Body() { page = 1, size = 10, token }) {
         const take = size
         const skip = (page - 1) * size
         const user = jwt.decode(token)
@@ -32,23 +30,24 @@ export class OrderController {
 
     @UseGuards(JwtAuthGuard, RolesGuard)
     @Roles(role.admin, role.customer)
-    @MessagePattern(ORDER_PATTERN.order_get_one)
-    async getOrder(id: string, token) {
+    @Post('get-order/:id')
+    async getOrder(@Param() id:string, @Req() req: any) {
+        const token = req.headers.authorization.split(' ')[1]
         const user = jwt.decode(token)
         return this.orderService.getOrder({ id, user })
     }
 
     @UseGuards(JwtAuthGuard, RolesGuard)
     @Roles(role.admin)
-    @MessagePattern(ORDER_PATTERN.order_update)
-    async updateOrder(data) {
+    @Post('update-order')
+    async updateOrder(@Body() data) {
         return this.orderService.updateOrder(data)
     }
 
     @UseGuards(JwtAuthGuard, RolesGuard)
     @Roles(role.admin)
-    @MessagePattern(ORDER_PATTERN.order_delete)
-    async deleteOrder(id: string) {
+    @Post('delete-order/:id')
+    async deleteOrder(@Param() id: string) {
         return this.orderService.deleteOrder(id)
     }
 }
